6cc0733e6b33bbc2f4962b9f7698dfddbd491022
João Duarte <joao.duarte@outlook.com>
2017-12-11 01:46:25 -0200

    first components

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,21 @@
-console.log('it works');
+import React from 'react';
+import { render } from 'react-dom';
+import { Header } from "./components/header";
+import { Footer } from "./components/footer";
+
+import { Home } from "./components/home";
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <Header />
+                <Footer />
+            </div>
+        );
+    }
+
+}
+
+render(<App />, window.document.getElementById("app")) 
