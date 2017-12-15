774e454a645bb24336d0f70f803118f8c96d0258
João Duarte <joao.duarte@outlook.com>
2017-12-15 08:33:02 -0200

    froala removed

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -2,18 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import { MyEditor } from "./editor";
 
-// Require Editor JS files.
-import 'froala-editor/js/froala_editor.pkgd.min.js';
-
-// Require Editor CSS files.
-import 'froala-editor/css/froala_style.min.css';
-import 'froala-editor/css/froala_editor.pkgd.min.css';
-
-// Require Font Awesome.
-import 'font-awesome/css/font-awesome.css';
-
-import FroalaEditor from 'react-froala-wysiwyg';
-
 var pptx = require('pptxgenjs');
 
 export class Home extends React.Component {
