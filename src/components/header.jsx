c797009350cb82043e88d0aa17f6607472c459fa
Wendler Eis <wendler_eis@hotmail.com>
2018-03-03 19:38:55 -0300

    Fixed: Navbar navigation links position

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,19 +3,24 @@ import React from 'react'
 class Header extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="">
-              <img alt="" src="../assets/img/logo.png" width="140" />
-            </a>
-          </div>
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav navbar-right">
-              <li><a href="">Explore</a></li>
-              <li><a href="">Doar</a></li>
-            </ul>
-          </div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <a className="navbar-brand" href="">
+          <img alt="" src="../assets/img/logo.png" width="140" />
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Explore</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-success" href="#">Doar</a>
+            </li>
+          </ul>
         </div>
       </nav>
     )
